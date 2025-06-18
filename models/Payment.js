5784432142b9c342f8bcaed8ae579b6f20f477cb
Aanchal7915 <aanchal2115@gmail.com>
2025-06-18 09:35:09 +0530

    modified model

--- models/Payment.js
+++ models/Payment.js
@@ -5,9 +5,9 @@ const paymentSchema = new mongoose.Schema(
     name: { type: String, required: true },
     phone: { type: String, required: true },
     razorpay_payment_id: { type: String, required: true },
-    razorpay_order_id: { type: String, required: true },
-    razorpay_signature: { type: String, required: true },
-    status: { type: String, enum: ["success", "failed"], required: true },
+    razorpay_order_id: { type: String},
+    razorpay_signature: { type: String},
+    status: { type: String, enum: ["success", "failed"] },
   },
   { timestamps: true }
 );
