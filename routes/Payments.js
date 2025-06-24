c42f81bafd4d08a9bfa2cceae2bf0316411a82a5
Aanchal7915 <aanchal2115@gmail.com>
2025-06-24 16:21:39 +0530

    solving bug

--- routes/Payments.js
+++ routes/Payments.js
@@ -5,6 +5,6 @@ const {authN, authZ} = require("../middleware/AuthNAuthZ");
 
 const { capturePayment, verifySignature } = require("../controllers/Payment");
 router.post("/create-order", capturePayment)
-router.post("/verifySignature", express.raw({ type: 'application/json' }), verifySignature)
+router.post("/verifySignature", verifySignature)
 
 module.exports = router
